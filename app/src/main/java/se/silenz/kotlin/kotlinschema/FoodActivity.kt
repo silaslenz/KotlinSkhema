e527830903ef2329aba66bbb2121e531296f114b
Silas Lenz <silas.lenz@gmail.com>
2016-01-30 15:26:20 +0100

    Getting food data

--- app/src/main/java/se/silenz/kotlin/kotlinschema/FoodActivity.kt
+++ app/src/main/java/se/silenz/kotlin/kotlinschema/FoodActivity.kt
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
+import com.github.kittinunf.fuel.Fuel
 import kotlinx.android.synthetic.main.content_food.*
 
 class FoodActivity : AppCompatActivity() {
@@ -29,11 +30,28 @@ class FoodActivity : AppCompatActivity() {
         // use a linear layout manager
         val mLayoutManager = LinearLayoutManager(this)
         my_recycler_view.layoutManager = mLayoutManager
-        val myDataset = arrayOf("test", "test2")
-
+        var myDataset = arrayOf("Loading")
         // specify an adapter (see also next example)
         val mAdapter = MyAdapter(myDataset)
         my_recycler_view.adapter = mAdapter
+
+        Fuel.get("http://skolmaten.se/katedralskolan2/").responseString { request, response, result ->
+            run {
+                val (d, e) = result
+                //                println("data is"+d)
+                //                println(d!!.split("<td class=\"date\">")?.get(1)?.split("<p class=\"item\">")[position+1]?.split("</p>")?.get(0))
+                mAdapter.clear()
+                for (i in 1..d!!.split("<td class=\"date\">").size - 1) {
+
+                    var dayStr = ""
+                    for (j in 1..d.split("<td class=\"date\">").get(i).split("<p class=\"item\">").size - 1) {
+                        dayStr += d.split("<td class=\"date\">").get(i).split("<p class=\"item\">")[j].split("</p>").get(0) + "\n"
+                    }
+                    mAdapter.add(d.split("<td class=\"date\">")[i].split("<span class=\"weekday\">")[1].split("</span>")[0], dayStr)
+                }
+            }
+        }
+
     }
 
 }
--- app/src/main/java/se/silenz/kotlin/kotlinschema/MyAdapter.kt
+++ app/src/main/java/se/silenz/kotlin/kotlinschema/MyAdapter.kt
@@ -1,33 +1,44 @@
 package se.silenz.kotlin.kotlinschema
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import se.silenz.kotlin.kotlinschema.R.layout.my_text_view
 
 /**
  * Created by silenz on 1/30/16.
  */
 class MyAdapter// Provide a suitable constructor (depends on the kind of dataset)
-(private val mDataset: Array<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+(private var mDataset: Array<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
                                     viewType: Int): MyAdapter.ViewHolder {
         // create a new view
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.my_text_view, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(my_text_view, parent, false)
         // set the view's size, margins, paddings and layout parameters
 
         val vh = ViewHolder(v as CardView)
+
         return vh
     }
 
+    var titleDataset = arrayOf("");
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
+
+
         holder.mTextView.text = mDataset[position]
+        holder.titleTextView.text = titleDataset[position]
+        holder.titleTextView.setTypeface(null, Typeface.BOLD)
+        holder.mTextView.setTextColor(Color.GRAY);
+
 
     }
 
@@ -36,15 +47,29 @@ class MyAdapter// Provide a suitable constructor (depends on the kind of dataset
         return mDataset.size
     }
 
+    fun clear() {
+        mDataset = arrayOf()
+        titleDataset = arrayOf()
+        notifyDataSetChanged()
+    }
+
+    fun add(title: String, content: String) {
+        mDataset = mDataset.plus(content)
+        titleDataset = titleDataset.plus(title)
+        notifyItemInserted(itemCount)
+    }
+
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     class ViewHolder(v: CardView) : RecyclerView.ViewHolder(v) {
         // each data item is just a string in this case
         var mTextView: TextView
+        var titleTextView: TextView
 
         init {
-            mTextView = v.getChildAt(0) as TextView
+            mTextView = v.findViewById(R.id.mTextView) as TextView
+            titleTextView = v.findViewById(R.id.titleTextView) as TextView
         }
     }
 
