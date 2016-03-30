9bdbe459452a392e7adb822d9fc111c4a38b99aa
Silas Lenz <silas.lenz@gmail.com>
2016-03-30 13:21:59 +0200

    Rename sitedata to pageContent for clarity

--- app/src/main/java/com/silenz/schema/AnalyzeNovaSoftwareSite.kt
+++ app/src/main/java/com/silenz/schema/AnalyzeNovaSoftwareSite.kt
@@ -3,14 +3,14 @@ package com.silenz.schema
 /**
  * Helper functions to get data from novasoftware pages.
  */
-fun hasDropdownselector(sitedata: String): Boolean {
-    return sitedata.split("<select name=\"ctl").size > 1
+fun hasDropdownselector(pageContent: String): Boolean {
+    return pageContent.split("<select name=\"ctl").size > 1
 }
 
-fun getUrlCode(sitedata: String): String {
-    return sitedata.split("WebViewer/").get(1).split("/printer")[0]
+fun getUrlCode(pageContent: String): String {
+    return pageContent.split("WebViewer/").get(1).split("/printer")[0]
 }
 
-fun getNumberOfDropdownItems(sitedata: String, type: String): Int {
-    return sitedata.split("name=\"" + type + "\"")[1].split("</select")[0].split("value=\"").size
+fun getNumberOfDropdownItems(pageContent: String, type: String): Int {
+    return pageContent.split("name=\"" + type + "\"")[1].split("</select")[0].split("value=\"").size
 }
