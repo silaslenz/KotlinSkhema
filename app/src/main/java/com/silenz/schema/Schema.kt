1ef04762888ea2de4a75b00e0172e32a80d4e4fd
Silas Lenz <silas.lenz@gmail.com>
2016-04-06 10:54:17 +0200

    Use correct week.

--- app/src/main/java/com/silenz/schema/Schema.kt
+++ app/src/main/java/com/silenz/schema/Schema.kt
@@ -7,7 +7,7 @@ import java.util.*
 class Schema(var schoolID: String, var userID: String) {
     fun getUrlThisWeek(context: Context): String {
         val display = (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay
-        val week = GregorianCalendar().get(Calendar.WEEK_OF_YEAR) + 1//TODO: Remove after debugging during easter
+        val week = GregorianCalendar().get(Calendar.WEEK_OF_YEAR)
         return "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=" + schoolID + "/sv-se&type=1&id=" + userID + "&period=&week=" + week + "&mode=0&printer=0&colors=32&head=0&clock=0&foot=0&day=0&width=" + display.width + "&height=" + display.height / 2
     }
 
