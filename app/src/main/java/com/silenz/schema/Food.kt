fec8c85e85af5ab790c3d1824e33623a6b1caf89
Silas Lenz <silas.lenz@gmail.com>
2017-02-19 13:20:48 +0100

    Fix food parsing

--- app/src/main/java/com/silenz/schema/Food.kt
+++ app/src/main/java/com/silenz/schema/Food.kt
@@ -7,30 +7,30 @@ import java.util.*
  * @param pageContent html from skolmaten.se
  * @param day index of day
  */
-fun getDateOfDay(pageContent: String, day: Int) = SimpleDateFormat("yyyy-mm-dd", Locale.getDefault()).parse(pageContent.split("<td class=\"date\">")[day].split("<span class=\"date\">")[1].split("</span>")[0])
+fun getDateOfDay(pageContent: String, day: Int) = SimpleDateFormat("yyyy-mm-dd", Locale.getDefault()).parse(pageContent.split("<span class=\"weekday\">")[day].split("<span class=\"date\">")[1].split("</span>")[0])
 
 /**
  * @param pageContent html from skolmaten.se
  */
-fun getDaysOnPage(pageContent: String) = pageContent.split("<td class=\"date\">").size
+fun getDaysOnPage(pageContent: String) = pageContent.split("<span class=\"weekday\">").size
 
 /**
  * @param pageContent html from skolmaten.se
  * @param day index of day
  */
-fun getNumberOfItemsOnDay(pageContent: String, day: Int) = pageContent.split("<td class=\"date\">")[day].split("<p class=\"item\">").size
+fun getNumberOfItemsOnDay(pageContent: String, day: Int) = pageContent.split("<span class=\"weekday\">")[day].split("<div class=\"items\">")[1].split("</div>")[0].split("<span>").size
 
 /**
  * @param pageContent html from skolmaten.se
  * @param day index of day
  * @param item index of item in day
  */
-fun getItemInDay(pageContent: String, day: Int, item: Int) = pageContent.split("<td class=\"date\">")[day].split("<p class=\"item\">")[item].split("</p>")[0] + "\n"
+fun getItemInDay(pageContent: String, day: Int, item: Int) = pageContent.split("<span class=\"weekday\">")[day].split("<span>")[item].split("</span>")[0] + "\n"
 
 /**
  * @param pageContent html from skolmaten.se
  * @param day index of day
  */
-fun getDaynameOfDay(day: Int, pageContent: String) = pageContent.split("<td class=\"date\">")[day].split("<span class=\"weekday\">")[1].split("</span>")[0]
+fun getDaynameOfDay(day: Int, pageContent: String) = pageContent.split("<span class=\"weekday\">")[day].split("</span>")[0]
 
 fun getWeekReason(pageContent: String) = pageContent.split("<span class=\"weekReason\">")[1].split("</")[0]
